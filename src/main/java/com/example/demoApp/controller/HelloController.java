4370f5a421495f815070a967eb12be578fd0ee66
zehrasbr <zehraisbr@gmail.com>
2023-12-21 20:30:23 +0300

    Initial commit

---
+++ src/main/java/com/example/demoApp/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demoApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    //localhost:8080/hello
+    @GetMapping(path = "/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}
