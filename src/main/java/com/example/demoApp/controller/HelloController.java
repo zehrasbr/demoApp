f90b74afe591966b63872df699e8408118f76d79
zehrasbr <zehraisbr@gmail.com>
2023-12-25 17:49:09 +0300

    update

--- src/main/java/com/example/demoApp/controller/HelloController.java
+++ src/main/java/com/example/demoApp/controller/HelloController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     //localhost:8080/hello
     @GetMapping(path = "/hello")
-
     public String sayHello() {
         return "Hello World!";
     }
